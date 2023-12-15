f7b58a0273d983326addc6ad4511932ac383e461
sameer <sam.anand619@gmail.com>
2023-12-15 11:57:42 +0530

    code formatting

--- src/component/DataTable/TableAction.tsx
+++ src/component/DataTable/TableAction.tsx
@@ -5,16 +5,14 @@ import styles from "./DataTable.module.css";
 
 type ModalOpen = "Edit" | "Delete" | false;
 
-interface ModalActionProps  {
+interface ModalActionProps {
   index: number;
   modal: ModalOpen;
   data: IDataItem;
   handleClose: () => void;
 }
 
-const ModalAction = ({modal, index, data, handleClose}:ModalActionProps) => {
-  
-
+const ModalAction = ({ modal, index, data, handleClose }: ModalActionProps) => {
   const [val, setVal] = useState<string>(data.name || "-");
   const { editData, deleteData } = useDataContext();
 
@@ -28,54 +26,46 @@ const ModalAction = ({modal, index, data, handleClose}:ModalActionProps) => {
     handleClose();
   };
 
-
-
-return <Modal open={!!modal}>
-  <div className={styles.modal}>
-    <p className={styles.modalHeader}>
-      {modal === "Edit" ? "Edit Name" : "Delete " + (index + 1)}
-    </p>
-    <form onSubmit={handleSubmit}>
-      {modal === "Edit" ? (
-        <input
-          className={styles.modalInput}
-          type="text"
-          value={val}
-          onChange={(e) => setVal(e.target.value)}
-        />
-      ) : null}
-      <div className={styles.modalButtons}>
-        <button className={styles.delete} onClick={handleClose}>
-          Cancel
-        </button>
-        <button className={styles.edit} type="submit">
-          {modal === "Edit" ? "Save" : "Confirm"}{" "}
-        </button>
+  return (
+    <Modal open={!!modal}>
+      <div className={styles.modal}>
+        <p className={styles.modalHeader}>
+          {modal === "Edit" ? "Edit Name" : "Delete " + (index + 1)}
+        </p>
+        <form onSubmit={handleSubmit}>
+          {modal === "Edit" ? (
+            <input
+              className={styles.modalInput}
+              type="text"
+              value={val}
+              onChange={(e) => setVal(e.target.value)}
+            />
+          ) : null}
+          <div className={styles.modalButtons}>
+            <button className={styles.delete} onClick={handleClose}>
+              Cancel
+            </button>
+            <button className={styles.edit} type="submit">
+              {modal === "Edit" ? "Save" : "Confirm"}{" "}
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-</Modal>
-
-}
+    </Modal>
+  );
+};
 
 type Props = {
   data: IDataItem;
   index: number;
 };
 
-
-
 function TableAction({ data, index }: Props) {
-  
-  
   const [modal, setModal] = useState<ModalOpen>(false);
 
   const handleClose = () => {
-
     setModal(false);
-  }
-  
- 
+  };
 
   return (
     <div>
@@ -88,7 +78,14 @@ function TableAction({ data, index }: Props) {
       >
         Delete
       </button>
-      {modal? <ModalAction modal={modal} index={index} data={data} handleClose={handleClose}/> : null}
+      {modal ? (
+        <ModalAction
+          modal={modal}
+          index={index}
+          data={data}
+          handleClose={handleClose}
+        />
+      ) : null}
     </div>
   );
 }
